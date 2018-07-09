ed00b1f12e9114ebfc67acdc0a4ac5008deafbe2
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-09 15:16:54 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Robot", "Robotics", 12000, "10/09/2018", "24/02/2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Laptop", "Electronics", 100000, "10/09/2018", "24/02/2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Phone", "Electronics", 10000, "10/09/2018", "24/02/2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Games Console", "Electronics", 10000, "10/09/2018", "24/02/2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Microwave", "Electronics", 10000, "10/09/2018", "22/02/2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Artwork", "Art", 1000, "10/09/2018", "24/02/2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Save the local park", "Community Project", 10000, "10/09/2018", "29/02/2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Tent", "Outdoors", 1000, "10/09/2018", "14/02/2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Tech Bed", "Furniture", 7000, "10/09/2018", "24/02/2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Stove", "Appliances", 4000, "10/09/2018", "18/02/2019");
