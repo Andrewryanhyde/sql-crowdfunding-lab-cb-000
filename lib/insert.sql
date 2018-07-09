273aaacb66b1502d6cbf2dd28c6b6897122892fe
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-09 15:20:58 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,3 +9,31 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Tent", "Outdoors", 1000, "10/09/2018", "14/02/2019");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Tech Bed", "Furniture", 7000, "10/09/2018", "24/02/2019");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Stove", "Appliances", 4000, "10/09/2018", "18/02/2019");
+
+
+INSERT INTO users (name, age) VALUES ("Robert", 92);
+INSERT INTO users (name, age) VALUES ("Samantha", 45);
+INSERT INTO users (name, age) VALUES ("Jenny",23 );
+INSERT INTO users (name, age) VALUES ("James", 34);
+INSERT INTO users (name, age) VALUES ("Dan", 56);
+INSERT INTO users (name, age) VALUES ("Bob", 12);
+INSERT INTO users (name, age) VALUES ("Sally", 36);
+INSERT INTO users (name, age) VALUES ("Julie", 42);
+INSERT INTO users (name, age) VALUES ("Chris", 65);
+INSERT INTO users (name, age) VALUES ("Jack", 34);
+INSERT INTO users (name, age) VALUES ("Lynda", 65);
+INSERT INTO users (name, age) VALUES ("Jennie", 45);
+INSERT INTO users (name, age) VALUES ("Sandy", 2);
+INSERT INTO users (name, age) VALUES ("Fred", 18);
+INSERT INTO users (name, age) VALUES ("Mildred", 23);
+INSERT INTO users (name, age) VALUES ("Margaret", 32);
+INSERT INTO users (name, age) VALUES ("Bernadette", 56);
+INSERT INTO users (name, age) VALUES ("Susan", 65);
+INSERT INTO users (name, age) VALUES ("Jo", 43);
+INSERT INTO users (name, age) VALUES ("Agatha", 34);
+
+
+
+
+
+
